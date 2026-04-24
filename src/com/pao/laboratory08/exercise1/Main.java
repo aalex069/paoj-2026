cf52971956f92f37467e1e4fa94251c2df3c7a9d
aalex069 <alexandru.telejman@gmail.com>
2026-04-24 19:06:38 +0300

    Am rezolvat exercitiul 1

--- src/com/pao/laboratory08/exercise1/Main.java
+++ src/com/pao/laboratory08/exercise1/Main.java
@@ -17,6 +17,61 @@ public class Main {
         //    - SHALLOW <nume> → shallow clone + modifică orașul clonei la "MODIFICAT" + afișează
         //    - DEEP <nume> → deep clone + modifică orașul clonei la "MODIFICAT" + afișează
 
-        System.out.println("TODO: implementează exercițiul 1");
+        List<Student> listaStudenti = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 4) {
+                    String nume = parts[0].trim();
+                    int varsta = Integer.parseInt(parts[1].trim());
+                    String oras = parts[2].trim();
+                    String strada = parts[3].trim();
+
+                    listaStudenti.add(new Student(nume, varsta, new Adresa(oras, strada)));
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Eroare");
+            return;
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] cmdParts = input.split(" ", 2);
+        String comanda = cmdParts[0];
+
+        if (comanda.equals("PRINT")) {
+            for (Student s : listaStudenti) {
+                System.out.println(s);
+            }
+        }
+        else if (comanda.equals("SHALLOW") || comanda.equals("DEEP")) {
+                String numeCautat = cmdParts[1];
+                Student original = null;
+
+                for (Student s : listaStudenti) {
+                    if (s.getNume().equals(numeCautat)) {
+                        original = s;
+                        break;
+                    }
+                }
+
+                if (original != null) {
+                    Student clona = (comanda.equals("SHALLOW")) ? original.shallowClone() : original.deepClone();
+
+                    clona.getAdresa().setOras("MODIFICAT");
+
+                    System.out.println("Original: " + original);
+                    System.out.println("Clona: " + clona);
+                } else {
+                    System.out.println("Studentul " + numeCautat + " nu a fost găsit.");
+                }
+            }
+
+        scanner.close();
     }
+        //System.out.println("TODO: implementează exercițiul 1");
 }
